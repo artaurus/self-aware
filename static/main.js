d38e721582c1e2fda1bcdcc69fb965238e42d4a8
Rishabh Ballal <rishabhballal@gmail.com>
2021-08-16 16:53:02 +0530

    mod: animations

--- static/main.js
+++ static/main.js
@@ -1,17 +1,22 @@
 document.querySelector('main').classList.add('hidden');
 const header = document.querySelector('header');
-header.classList.remove('hidden');
-var opacity = 100;
 setTimeout(() => {
-  const int = setInterval(() => {
-    header.style.opacity = --opacity*0.01;
-    if (!opacity) {
-      clearInterval(int);
-      header.classList.add('hidden');
-      document.querySelector('main').classList.remove('hidden');
-    }
-  }, 10);
-}, 1600);
+  document.querySelector('figure.om').classList.remove('hidden');
+  setTimeout(() => {
+    document.querySelector('h1').classList.remove('hidden');
+    var opacity = 100;
+    setTimeout(() => {
+      const int = setInterval(() => {
+        header.style.opacity = --opacity*0.01;
+        if (!opacity) {
+          clearInterval(int);
+          header.classList.add('hidden');
+          document.querySelector('main').classList.remove('hidden');
+        }
+      }, 10);
+    }, 2000);
+  }, 1000);
+}, 1200);
 
 document.querySelector('button.toggle').classList.remove('hidden');
 const btns = document.querySelectorAll('button.toggle');
